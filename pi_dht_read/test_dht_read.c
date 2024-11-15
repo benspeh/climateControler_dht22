a43b8cd2405940d1cd46c63939121d2db1261b50
benspeh <benspeh@googlemail.com>
2024-11-16 00:47:03 +0100

    Update test_dht_read.c

--- pi_dht_read/test_dht_read.c
+++ pi_dht_read/test_dht_read.c
@@ -3,20 +3,22 @@
 #include "pi_dht_read.h"
 
 // GPIO pin number for DHT sensor
-#define DHTPIN 4
+#define DHTPIN 24
 
 int main(int argc, const char **argv) {
-	float humidity, temperature;
-	int count = argc < 2 ? 1 : atoi(argv[1]);
-	while (count-- > 0) {
-		int success = dht_read(AM2302, DHTPIN, &humidity, &temperature);
-		if (success) {
-			printf("temperature:%.1f Humidity:%.1f\n", temperature, humidity);
-		}
-		if (count > 0) {
-			printf("Press ENTER key to continue\n");
-			getchar();
-		}
-	}
-	return 0;
+        float humidity, temperature;
+        int count = argc < 2 ? 1 : atoi(argv[1]);
+        while (count-- > 0) {
+                int success = dht_read(AM2302, DHTPIN, &humidity, &temperature);
+                if (success) {
+                        printf("temperature:%.1f Humidity:%.1f\n", temperature, humidity);
+                } else {
+                printf("Failed to read sensor!\n");
+                }
+        printf("Wait 1min...\n");
+        sleep(60);
+        }
+
+        return 0;
 }
+
