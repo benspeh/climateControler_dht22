0542d1ba37f2c73471ef50e1f41f2804edbe5939
benspeh <benspeh@googlemail.com>
2024-11-18 20:44:53 +0100

    Create pi_readDHT.hpp

---
+++ pi_readDHT/pi_readDHT.hpp
@@ -0,0 +1,20 @@
+#ifndef PI_DHT_READ_HPP
+#define PI_DHT_READ_HPP
+
+// Define sensor types
+#define DHT11 11
+#define DHT22 22
+#define AM2302 22
+
+/**
+ * Read humidity and temperature from the Adafruit DHT sensor, with retries.
+ *
+ * @param type Sensor type (e.g., AM2302)
+ * @param pin GPIO pin number (e.g., 4)
+ * @param pHumidity Pointer to a float where humidity is set on return
+ * @param pTemperature Pointer to a float where temperature is set on return
+ * @return 1 if successful, 0 if failed
+ */
+int dht_read(int type, int pin, float* pHumidity, float* pTemperature);
+
+#endif  // PI_DHT_READ_HPP
