912f06b6131321066535d81aa9c0ba4f193040dc
benspeh <benspeh@googlemail.com>
2024-11-18 21:25:40 +0100

    Update dht_readLOOP.cpp

--- pi_readDHT/dht_readLOOP.cpp
+++ pi_readDHT/dht_readLOOP.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>  // for sleep()
-#include "pi_dht_read.h"
+#include "pi_dht_read.hpp"
 
 // GPIO pin number for DHT sensor
 #define DHTPIN 24
