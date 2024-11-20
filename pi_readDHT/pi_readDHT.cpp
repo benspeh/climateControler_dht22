35d1bb6ac9a75c03e9e41097f33a1244f7754b8c
benspeh <benspeh@googlemail.com>
2024-11-20 21:34:40 +0100

    Update pi_readDHT.cpp

--- pi_readDHT/pi_readDHT.cpp
+++ pi_readDHT/pi_readDHT.cpp
@@ -210,7 +210,7 @@ int dht_read(int type, int pin, float* pHumidity, float* pTemperature) {
                 }
             }
             if (count > 0) {
-                sleep_milliseconds(1500);
+                sleep(2);
             }
         }
         if (lockfd >= 0) {
