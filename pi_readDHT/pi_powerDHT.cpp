e889bb4c5b1896ed286c396343b3a28e1a496be5
benspeh <benspeh@googlemail.com>
2024-11-21 23:32:55 +0100

    Update pi_powerDHT.cpp

--- pi_readDHT/pi_powerDHT.cpp
+++ pi_readDHT/pi_powerDHT.cpp
@@ -5,19 +5,7 @@
 #include <string>      // std::string
 
 #include "pi_powerDHT.hpp"
-
-std::string getLogHeader() {
-    char buff[64];
-    time_t timeNow = time(nullptr);
-    struct tm tmNow;
-    localtime_r(&timeNow, &tmNow);
-    snprintf(buff, sizeof(buff), "%04d-%02d-%02d %02d:%02d:%02d dht_read: ",
-             tmNow.tm_year + 1900, tmNow.tm_mon + 1, tmNow.tm_mday,
-             tmNow.tm_hour, tmNow.tm_min, tmNow.tm_sec);
-    return std::string(buff);
-}
-
-#define DHT_READ_LOG(fmt, ...) printf("%s" fmt, getLogHeader().c_str(), ##__VA_ARGS__)
+#include "pi_LOGGING.hpp"
 
 void dht_initPowerPin(int pin) {
 
