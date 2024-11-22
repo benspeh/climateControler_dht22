147ce671a7e6fda5d00fe67788fe140c07b67c82
benspeh <benspeh@googlemail.com>
2024-11-22 21:13:06 +0100

    Update setup_readDHT22_service.sh

--- serviceFiles/setup_readDHT22_service.sh
+++ serviceFiles/setup_readDHT22_service.sh
@@ -1,15 +1,8 @@
+sudo cp readDHT22.service /etc/systemd/system/readDHT22.service
 
-sudo systemctl stop dynamicI2Cooling.service
-
-sudo docker run --rm -v "$PWD":/repo -w /repo alpine/git reset --hard
-sudo docker run --rm -v "$PWD":/repo -w /repo alpine/git pull origin main
-
-sudo cp readDHT22.service /etc/systemd/system/dynamicI2Cooling.service
-
-sudo chmod +x /etc/systemd/system/dynamicI2Cooling.service
-sudo chmod +x ./dynamicI2CoolingRasPi.sh
-sudo chmod +x ./update_dynamicI2CoolingService.sh
+sudo chmod +x /etc/systemd/system/readDHT22.service
 
 sudo systemctl daemon-reload
-sudo systemctl restart dynamicI2Cooling.service
+sudo systemctl enable dynamicI2Cooling.service
+sudo systemctl start dynamicI2Cooling.service
 sudo systemctl status dynamicI2Cooling.service
