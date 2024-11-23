ae701b14114adda808a40e55cd9d9c809cc07628
benspeh <benspeh@googlemail.com>
2024-11-23 01:30:45 +0100

    Update setup_readDHT22_service.sh

--- serviceFiles/setup_readDHT22_service.sh
+++ serviceFiles/setup_readDHT22_service.sh
@@ -3,6 +3,6 @@ sudo cp readDHT22.service /etc/systemd/system/readDHT22.service
 sudo chmod +x /etc/systemd/system/readDHT22.service
 
 sudo systemctl daemon-reload
-sudo systemctl enable dynamicI2Cooling.service
-sudo systemctl start dynamicI2Cooling.service
-sudo systemctl status dynamicI2Cooling.service
+sudo systemctl enable readDHT22.service
+sudo systemctl start readDHT22.service
+sudo systemctl status readDHT22.service
