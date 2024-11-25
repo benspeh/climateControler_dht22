aea0bc0e1bb3a036de5314a9926ecb4d0bf6ec3a
benspeh <benspeh@googlemail.com>
2024-11-25 01:16:39 +0100

    Create update_readDHT22_service.sh

---
+++ serviceFiles/update_readDHT22_service.sh
@@ -0,0 +1,7 @@
+sudo cp readDHT22.service /etc/systemd/system/readDHT22.service
+
+sudo chmod +x /etc/systemd/system/readDHT22.service
+
+sudo systemctl daemon-reload
+sudo systemctl restart readDHT22.service
+sudo systemctl status readDHT22.service
