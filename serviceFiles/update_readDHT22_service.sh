77398454a40d6ff7ebac97bb11a20cf690fd65c8
benspeh <benspeh@googlemail.com>
2024-11-25 01:21:20 +0100

    Update update_readDHT22_service.sh

--- serviceFiles/update_readDHT22_service.sh
+++ serviceFiles/update_readDHT22_service.sh
@@ -1,7 +1,9 @@
+sudo systemctl stop readDHT22.service
+
 sudo cp readDHT22.service /etc/systemd/system/readDHT22.service
 
 sudo chmod +x /etc/systemd/system/readDHT22.service
 
 sudo systemctl daemon-reload
-sudo systemctl restart readDHT22.service
+sudo systemctl start readDHT22.service
 sudo systemctl status readDHT22.service
