d3a2d2d9904375e1641e4c6b5c9f243a1162b0cf
benspeh <benspeh@googlemail.com>
2024-11-26 23:04:08 +0100

    Update climateModel_VPD.hpp

--- climateModel_VPD/climateModel_VPD.hpp
+++ climateModel_VPD/climateModel_VPD.hpp
@@ -2,7 +2,9 @@
 #define PI_POWER_DHT_HPP
 
 // Declare the functions
-void dht_initPowerPin(int pin);
-void dht_resetPowerPin(int pin);
+// Function to calculate the saturation vapor pressur
+double saturationVaporPressure(double temperature);
+// Function to calculate the VPD
+double calculateVPD(double temperature, double relativeHumidity);
 
 #endif // PI_POWER_DHT_HPP
