8105b395faa29eec7923161fbb2daac67da172d9
benspeh <benspeh@googlemail.com>
2024-11-28 02:20:57 +0100

    Update update_climateControler_dht22.sh

--- update_climateControler_dht22.sh
+++ update_climateControler_dht22.sh
@@ -3,14 +3,13 @@ sudo docker run --rm -v "$PWD":/repo -w /repo alpine/git reset --hard && sudo do
 sudo chmod +x ./serviceFiles/setup_readDHT22_service.sh
 sudo chmod +x ./update_climateControler_dht22.sh
 
-cd ./pi_readDHT
-
 sudo docker run --rm \
   --privileged \
   -v "$PWD":/workspace \
   -w /workspace \
-  ubuntu bash -c "apt update && apt install -y build-essential make && make clean && make"
+  ubuntu bash -c "apt update && apt install -y build-essential make && cd ./pi_readDHT && make clean && make && cd ../ && cd ./climateModel_VPD && make clean && make"
 
-cd ../
+
 cd ./serviceFiles
 sudo ./update_readDHT22_service.sh
+cd ../
