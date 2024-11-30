e05fd0b4a22fdde5b9e0a68665138a67412f4029
benspeh <benspeh@googlemail.com>
2024-11-30 02:35:56 +0100

    Update climateModel_VPD.hpp

--- climateModel_VPD/climateModel_VPD.hpp
+++ climateModel_VPD/climateModel_VPD.hpp
@@ -1,5 +1,5 @@
-#ifndef CLIMATE_MODEL_VPD_HPP
-#define CLIMATE_MODEL_VPD_HPP
+#ifndef CLIMATEMODEL_VPD_HPP
+#define CLIMATEMODEL_VPD_HPP
 
 // Declare the functions
 // Function to calculate the saturation vapor pressur
@@ -7,4 +7,4 @@ double saturationVaporPressure(double temperature);
 // Function to calculate the VPD
 double calculateVPD(double temperature, double relativeHumidity);
 
-#endif // CLIMATE_MODEL_VPD_HPP
+#endif // CLIMATEMODEL_VPD_HPP
